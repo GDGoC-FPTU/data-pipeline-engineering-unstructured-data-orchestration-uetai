0bc4ed981b08bb70cfa71477649f97de684cbd75
Nguyễn Minh HIếu <minhhieutrumhoa1@gmail.com>
2026-04-24 10:41:55 +0700

    Complete pipeline and generate results

--- starter_code/orchestrator.py
+++ starter_code/orchestrator.py
@@ -24,10 +24,13 @@ def run_pipeline():
         with open(file_path, 'r') as f:
             raw_data = json.load(f)
         
-        # TODO: Bước 1: Gọi hàm xử lý PDF (process_pdf_data)
+        # Bước 1: Gọi hàm xử lý PDF (process_pdf_data)
+        doc_dict = process_pdf_data(raw_data)
         
-        # TODO: Bước 2: Kiểm tra chất lượng (run_semantic_checks). 
+        # Bước 2: Kiểm tra chất lượng (run_semantic_checks). 
         # Nếu đạt (True) thì thêm vào list final_kb
+        if run_semantic_checks(doc_dict):
+            final_kb.append(UnifiedDocument(**doc_dict).model_dump())
 
     # Xử lý Group B (Videos)
     video_files = glob.glob(os.path.join(RAW_DATA_DIR, "group_b_videos", "*.json"))
@@ -35,7 +38,10 @@ def run_pipeline():
         with open(file_path, 'r') as f:
             raw_data = json.load(f)
         
-        # TODO: Làm tương tự như phần PDF (gọi hàm xử lý Video và kiểm tra chất lượng)
+        # Làm tương tự như phần PDF
+        doc_dict = process_video_data(raw_data)
+        if run_semantic_checks(doc_dict):
+            final_kb.append(UnifiedDocument(**doc_dict).model_dump())
 
     # Lưu kết quả
     with open(OUTPUT_FILE, 'w') as f:
